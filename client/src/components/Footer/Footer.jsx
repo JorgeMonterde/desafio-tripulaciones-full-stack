e1e0ac2bccf2c820797af68b371bd9874ac6f13d
Mariangelica Rodriguez <gromarant@gmail.com>
2023-07-26 21:47:04 +0200

    update/footer_styles

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,19 +1,18 @@
+import { AiFillFacebook, AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
+
 const Footer = () => {
   return (
     <footer className='footer'>
-      <ul>
+      <ul className='footer_links'>
         <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>Avisos legales</a></li>
         <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>Política de privacidad</a></li>
         <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>Política de cookies</a></li>
-      </ul>
-      <ul>
-        <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>RRSS/icon</a></li>
-        <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>RRSS/icon</a></li>
-        <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>RRSS/icon</a></li>
-      </ul>
-      <ul>
         <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>FAQs</a></li>
-        <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'>Certificaciones</a></li>
+      </ul>
+      <ul className='footer_RRSS'>
+        <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'><AiFillFacebook  className='icon'/></a></li>
+        <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'><AiOutlineInstagram className='icon' /></a></li>
+        <li><a href='https://github.com/desafioteam1/full-stack/tree/develop'><AiOutlineTwitter className='icon' /></a></li>
       </ul>
     </footer>
   );
