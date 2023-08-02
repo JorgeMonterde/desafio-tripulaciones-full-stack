fdb8350228bc57b07013a61052cca1bfcdfcd547
Mariangelica Rodriguez <gromarant@gmail.com>
2023-08-02 03:33:59 +0200

    update/About_page__styles

--- client/team.js
+++ client/team.js
@@ -1,32 +1,62 @@
 export const team = [
   {
-    "img": "../../../../public/assets/jorgePicture.png",
+    "img": "../../../../public/assets/About/jorgePicture.png",
     "name": "Jorge Monterde",
     "role": "Desarrollador",
     "rrss": "https://www.linkedin.com/in/jorge-monterde-ortega-6aa50127b/"
   },
   {
-    "img": "../../../../public/assets/anaPicture.png",
+    "img": "../../../../public/assets/About/anaPicture.png",
     "name": "Annita Niño",
     "role": "Diseñadora UX/UI",
     "rrss": "annitaing@gmail.com"
   },
   {
-    "img": "../../../../public/assets/pabloPicture.jpg",
+    "img": "../../../../public/assets/About/pabloPicture.jpg",
     "name": "Pablo Primo Rodriguez",
     "role": "Experto Cloud",
     "rrss": "https://www.linkedin.com/in/pabloprimo/"
   },
   {
-    "img": "../../../../public/assets/juditPicture.png",
+    "img": "../../../../public/assets/About/juditPicture.jpg",
     "name": "Judit Roca Tovar",
     "role": "Científica de datos",
     "rrss": "https://www.linkedin.com/in/judit-r-ab8734b0/"
   },
   {
-    "img": "../../../../public/assets/marianPicture.png",
+    "img": "../../../../public/assets/About/marianPicture.png",
     "name": "Mariangelica Rodríguez",
     "role": "Desarrolladora",
     "rrss": "https://www.linkedin.com/in/mariangelicarodriguezperez/"
+  },
+  {
+    "img": "../../../../public/assets/About/claudioPicture.jpg",
+    "name": "Claudio Haraba",
+    "role": "Desarrolladora",
+    "rrss": "https://www.linkedin.com/in/claudioharaba/"
+  },
+  {
+    "img": "../../../../public/assets/About/albertoPicture.jpg",
+    "name": "Alberto Jiménez Báez",
+    "role": "Experto Cloud",
+    "rrss": "https://www.linkedin.com/in/albertojimenezbaez/"
+  },
+  {
+    "img": "../../../../public/assets/About/christianPicture.jpg",
+    "name": "Christian de López",
+    "role": "experto en Ciberseguridad",
+    "rrss": "https://www.linkedin.com/in/christiandelopez/"
+  },
+  {
+    "img": "../../../../public/assets/About/rominaPicture.png",
+    "name": "Romina Lupas",
+    "role": "experto en Ciberseguridad",
+    "rrss": "https://www.linkedin.com/in/gabriela-romina-lupas"
+  },
+  {
+    "img": "../../../../public/assets/About/claraPicture.jpg",
+    "name": "Clara García",
+    "role": "experto en Marketing",
+    "rrss": ""
   }
 ];
